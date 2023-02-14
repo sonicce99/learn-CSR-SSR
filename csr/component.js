e5dc8dbdba449a9e10e0960562c297265256a685
dongsu <sonicce99@naver.com>
2023-02-14 12:28:59 +0900

    feat : component.js 분리

---
+++ csr/component.js
@@ -0,0 +1,5 @@
+export const Content = `
+<h1>CSR을 배워보자</h1>
+<div id='canvas' style='width: 300px; height: 200px; border: 3px solid red;'></div>
+<button id='btn'>배경 바꾸기</button>
+`;
--- csr/index.js
+++ csr/index.js
@@ -1,23 +1,21 @@
-const $root = document.getElementById("root");
-
-const content = `
-<h1>CSR을 배워보자</h1>
-<div id='canvas' style='width: 300px; height: 200px; border: 3px solid red;'></div>
-<button id='btn'>배경 바꾸기</button>
-`;
-
-$root.innerHTML = content;
+import { Content } from "./component.js";
 
 let isColorGreen = false;
-const $btn = document.getElementById("btn");
-$btn.addEventListener("click", () => {
-  const $canvas = document.getElementById("canvas");
 
-  if (isColorGreen) {
-    $canvas.style.backgroundColor = "red";
-  } else {
-    $canvas.style.backgroundColor = "green";
-  }
+const render = () => {
+  const $root = document.getElementById("root");
+  $root.innerHTML = Content;
+  $root.querySelector("button").addEventListener("click", () => {
+    const $canvas = document.getElementById("canvas");
 
-  isColorGreen = !isColorGreen;
-});
+    if (isColorGreen) {
+      $canvas.style.backgroundColor = "red";
+    } else {
+      $canvas.style.backgroundColor = "green";
+    }
+
+    isColorGreen = !isColorGreen;
+  });
+};
+
+render();
