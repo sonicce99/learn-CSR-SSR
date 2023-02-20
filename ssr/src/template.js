92864e1c788aef18e967d1af695de4dad593d405
dongsu <sonicce99@naver.com>
2023-02-20 09:40:28 +0900

    docs : stylesheet 추가

--- ssr/src/template.js
+++ ssr/src/template.js
@@ -6,6 +6,7 @@ module.exports = `<!DOCTYPE html>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="./style.css" />
     <title>SSR | 동수의 로그북</title>
   </head>
   <body>
